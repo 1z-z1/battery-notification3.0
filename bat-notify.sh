844791d7242ea3fcc5dd74a81795a57fa91e7971
Vladimir Kirov <vladimir.kirov@siteground.com>
2020-04-21 07:14:52 +0300

    rewrite with functions and case

--- bat-notify.sh
+++ bat-notify.sh
@@ -4,8 +4,7 @@
 
 i=0
 
-while true
-do
+while true; do
 
 BatteryCapacity=$(cat /sys/class/power_supply/BAT0/capacity)
 BatteryStatus=$(cat /sys/class/power_supply/BAT0/status)
@@ -16,36 +15,37 @@ CriticalAlert=10
 NormalAlert=15
 LowAlert=95
 
-  if [[ "$BatteryStatus" == "$BatteryDischarging" ]]; then
-    if [[ $BatteryCapacity -le $CriticalAlert ]]; then
-      ((i=0))
-      notify-send -u critical "Alert" "Battery Low!!!"
-      sleep 30
-    elif [[ $BatteryCapacity -le $NormalAlert ]]; then
-      ((i=0))
-      nofify-send -u normal "Alert" "Battery getting low..."
-      sleep 60
-    elif [[ $BatteryCapacity -le $LowAlert ]]; then
-      ((i=0))
-      notify-send -u low "Alert" "Think about plugging in computer if possible..."
-      sleep 300
-    fi
-  elif [[ "$BatteryStatus" == "$BatteryFull" ]]; then
-    if [[ "${i}" -eq 0 ]]; then
-      notify-send -u normal "Alert" "Battery fully charged."
-      ((i++))
-    fi
-  elif [[ "$BatteryStatus" == "$BatteryCharging" ]]; then
-    if [[ "${i}" -eq 0 ]]; then
-      sleep 2
-    fi
-  elif [[ "$BatteryStatus" == "$BatteryCharging" ]]; then
-    if [[ $BatteryCapacity -le 80 ]]; then
-      ((i=0))
-    sleep 2
-    fi
-  else
-    ((i=1))
-    sleep 2
+function Discharging () {
+  i=0
+  if [[ $BatteryCapacity -le $CriticalAlert ]]; then
+    notify-send -u critical "Alert" "Battery Low!!!"
+    sleep 30
+  elif [[ $BatteryCapacity -le $NormalAlert ]]; then
+    nofify-send -u normal "Alert" "Battery getting low..."
+    sleep 60
+  elif [[ $BatteryCapacity -le $LowAlert ]]; then
+    notify-send -u low "Alert" "Think about plugging in computer if possible..."
+    sleep 300
   fi
-done
+}
+
+function Full () {
+  if [[ "${i}" -eq 0 ]]; then
+    notify-send -u normal "Alert" "Battery fully charged."
+    ((i++))
+  fi
+}
+
+function Charging () {
+  if [[ "${i}" -ne 0 ]] && [[ $BatteryCapacity -le 80 ]]; then
+    i=0
+  fi
+  sleep 2
+}
+
+case "${BatteryStatus}" in
+   "${BatteryDischarging}") Discharging;;
+          "${BatteryFull}") Full;;
+      "${BatteryCharging}") Charging;;
+                         *) i=1; sleep 2;;
+esac
